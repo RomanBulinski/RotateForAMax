99bdf698d86499236d699e227445a014660bf9a3
Roman Bulinski <aboandrus@gmail.com>
2019-04-17 17:52:47 +0200

    Code refactor

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,22 +1,21 @@
 public class Main {
 
-    public static long maxRot (long n) {
+    public static long maxRot(long n) {
 
         long result = 0;
         String aNum = String.valueOf(n);
         int counter = 0;
-        String tempString= aNum;
+        String tempString = aNum;
 
-        while( counter <= aNum.length()-1 ){
-            if( Long.valueOf( tempString  )  > result ){
-                result = Long.valueOf( tempString  ) ;
+        while (counter <= aNum.length() - 1) {
+            if (Long.valueOf(tempString) > result) {
+                result = Long.valueOf(tempString);
             }
-            tempString = tempString.substring(0,counter)  + tempString.substring(counter+1) + tempString.substring(counter,counter+1);
+            tempString = tempString.substring(0, counter) + tempString.substring(counter + 1) + tempString.substring(counter, counter + 1);
             counter++;
         }
         return result;
     }
-
 
 
 }
